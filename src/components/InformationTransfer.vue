3d1d4f483b129bc021cb0cdd7ced82d7934f2258
EvgeniyLV <zhenia.larionoff2015@yandex.ru>
2022-08-09 16:17:49 +0300

    стили для блока methods

--- src/components/InformationTransfer.vue
+++ src/components/InformationTransfer.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="information-transfer">
-    <img
-      class="information-transfer__image-bg"
-      src="../assets/images/information-transfer-bg.svg"
-      alt=""
-    />
     <div class="methods">
       <img class="methods__image" src="../assets/images/info.svg" alt="" />
-      <h3 class="title">Способы передачи информации о заказах:</h3>
+      <h3 class="methods__title">Способы передачи информации о заказах:</h3>
       <div class="methods-cards">
         <div class="methods-card">
           <p class="methods-card__text">Как</p>
@@ -19,13 +14,14 @@
           <p class="methods-card__text">Telegram</p>
           <p class="methods-card__text">WhatsApp</p>
           <p class="methods-card__text">
-            На электронную почту order@dostaver.com
+            На электронную почту <br />
+            order@dostaver.com
           </p>
         </div>
       </div>
     </div>
 
-    <div class="revenue-report">
+    <!-- <div class="revenue-report">
       <div class="revenue-report-item">
         <img
           class="revenue-report-item__image"
@@ -54,7 +50,7 @@
           пример отчёта
         </a>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -66,5 +62,83 @@ export default {
 
 <style lang="scss">
 .information-transfer {
+  min-height: 926px;
+  margin: 78px 0 0;
+  padding: 0 30px;
+  background-image: url(../assets/images/information-transfer-bg.svg);
+  background-repeat: no-repeat;
+  background-position: center bottom;
+
+  @media screen and (max-width: $laptop-small) {
+    margin: 51px 0 0;
+    background-image: none;
+  }
+}
+
+.information-transfer .methods {
+  &__image {
+    display: block;
+    margin: 0 auto;
+  }
+
+  &__title {
+    margin: 32px 0 0;
+    font-weight: 600;
+    font-size: 28px;
+    line-height: 30px;
+    text-align: center;
+
+    @media screen and (max-width: $laptop-small) {
+      font-size: 24px;
+    }
+  }
+
+  .methods-cards {
+    max-width: 852px;
+    margin: 45px auto 0;
+    display: flex;
+    justify-content: space-between;
+
+    @media screen and (max-width: $laptop-small) {
+      display: block;
+      margin: 23px auto 0;
+      max-width: 600px;
+    }
+  }
+
+  .methods-cards .methods-card {
+    width: calc(50% - 15px);
+
+    @media screen and (max-width: $laptop-small) {
+      width: 100%;
+      margin: 0 auto;
+    }
+
+    &:last-child {
+      @media screen and (max-width: $laptop-small) {
+        margin: 20px auto 0;
+      }
+    }
+
+    &__text {
+      padding: 26px 0;
+      font-weight: 500;
+      font-size: 18px;
+      line-height: 24px;
+      border: 1px solid #ffffff;
+      text-align: center;
+
+      &:first-child {
+        border-radius: 38px 38px 0 0;
+        padding: 14px 0;
+        background: $color-white;
+        color: $color-dark-purple;
+      }
+
+      &:last-child {
+        border-radius: 0 0 38px 38px;
+      }
+    }
+  }
 }
 </style>
