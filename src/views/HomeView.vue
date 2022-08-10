53f288c7ebce07eec8023a052fa574702404904a
EvgeniyLV <zhenia.larionoff2015@yandex.ru>
2022-08-10 21:39:18 +0300

    общие правки

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <!-- <Header /> -->
     <Banner />
     <Price />
     <Organization />
@@ -8,12 +7,10 @@
     <Status />
     <About />
     <Application />
-    <!-- <Footer /> -->
   </div>
 </template>
 
 <script>
-//
 import Banner from "@/components/Banner.vue";
 import Price from "@/components/Price.vue";
 import Organization from "@/components/Organization.vue";
@@ -21,12 +18,10 @@ import Money from "@/components/Money.vue";
 import Status from "@/components/Status.vue";
 import About from "@/components/About.vue";
 import Application from "@/components/Application.vue";
-//import Footer from "@/components/Footer.vue";
 
 export default {
   name: "HomeView",
   components: {
-    //Header,
     Banner,
     Price,
     Organization,
@@ -34,7 +29,6 @@ export default {
     Status,
     About,
     Application,
-    //Footer,
   },
 };
 </script>
